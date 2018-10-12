96b4719313a7f325db3b213d4cef411739a8de7f
Pravin Arote <pravin.arote@trafigura.com>
2018-10-12 19:44:27 +0530

    Nameof fix

--- TaskManager/TaskManager.PerformanceTests/PerformanceTestSuite.cs
+++ TaskManager/TaskManager.PerformanceTests/PerformanceTestSuite.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -40,4 +41,17 @@ namespace TaskManager.PerformanceTests
             }
         }
     }
+
+    public class nameof<T>
+    {
+        public static string Property<TProp>(Expression<Func<T, TProp>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+            if (body == null)
+                throw new ArgumentException("'expression' should be a member expression");
+            return body.Member.Name;
+        }
+    }
+
+
 }
