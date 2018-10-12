7055ef1e95fa5f943417aad6bf69938b3c16d833
Pravin Arote <pravin.arote@trafigura.com>
2018-10-12 19:57:42 +0530

    Revert

--- TaskManager/TaskManager.PerformanceTests/PerformanceTestSuite.cs
+++ TaskManager/TaskManager.PerformanceTests/PerformanceTestSuite.cs
@@ -42,16 +42,4 @@ namespace TaskManager.PerformanceTests
         }
     }
 
-    public class nameof<T>
-    {
-        public static string Property<TProp>(Expression<Func<T, TProp>> expression)
-        {
-            var body = expression.Body as MemberExpression;
-            if (body == null)
-                throw new ArgumentException("'expression' should be a member expression");
-            return body.Member.Name;
-        }
-    }
-
-
 }
